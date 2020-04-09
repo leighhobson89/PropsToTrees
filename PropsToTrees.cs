b7f94ca36079c1d956c147694fe9c80ac02d162a
Leigh Hobson <leighhobson89@outlook.com>
2020-04-09 11:55:08 +0200

    First Push

---
+++ PropsToTrees.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ICities;
+using UnityEngine;
+
+namespace PropsToTrees
+{
+    public class PropsToTrees : IUserMod
+    {
+        public string Name
+        {  
+            get { return "PropsToTrees"; }
+        }
+
+        public string Description
+        {
+            get { return "My first mode to convert Props to Trees"; }
+        }
+    }
+}
